c524c5a9ca385189114c0eebb3d114071eff9442
WSokoll <wladek.sokol01@gmail.com>
2023-04-22 14:06:33 +0200

    Vue base - custom

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img class="img-fluid px-4 py-4" alt="Logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome"/>
 </template>
 
 <script>
