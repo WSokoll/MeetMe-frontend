92a9bf895db9261c27893da3443b84af471ee3bb
WSokoll <wladek.sokol01@gmail.com>
2023-04-23 17:58:01 +0200

    Login + Register views, styling

---
+++ src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="form-section">
+    <div class="form-box form-box-login">
+      <div class="form-value">
+        <form action="">
+          <h2 class="form-title">Login</h2>
+          <div class="inputbox">
+            <i class="bi bi-person form-icon"></i>
+            <input type="text" placeholder=" " required>
+            <label for="">Username</label>
+          </div>
+          <div class="inputbox">
+            <i class="bi bi-lock form-icon"></i>
+            <input type="password" placeholder=" " required>
+            <label for="">Password</label>
+          </div>
+          <button class="form-button">Log in</button>
+          <div class="register-link">
+            <p>Don't have an account? <router-link to="/register">Register</router-link></p>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LoginView'
+}
+</script>
+
+<style src="@/assets/css/forms.css"></style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="form-section">
+      <div class="form-box form-box-register">
+        <div class="form-value">
+          <form action="">
+            <h2 class="form-title">Register</h2>
+            <div class="inputbox">
+              <i class="bi bi-person form-icon"></i>
+              <input type="text" placeholder=" " required>
+              <label for="">Username</label>
+            </div>
+            <div class="inputbox">
+              <i class="bi bi-person form-icon"></i>
+              <input type="text" placeholder=" " required>
+              <label for="">First name</label>
+            </div>
+            <div class="inputbox">
+              <i class="bi bi-person form-icon"></i>
+              <input type="text" placeholder=" " required>
+              <label for="">Last name</label>
+            </div>
+            <div class="inputbox">
+            <i class="bi bi-envelope-at form-icon"></i>
+              <input type="email" placeholder=" " required>
+              <label for="">Email</label>
+            </div>
+            <div class="inputbox">
+              <i class="bi bi-lock form-icon"></i>
+              <input type="password" placeholder=" " required>
+              <label for="">Password</label>
+            </div>
+            <button class="form-button">Register</button>
+            <div class="login-link">
+              <p>Already have an account? <router-link to="/login">Log in</router-link></p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+<script>
+  export default {
+    name: 'RegisterView'
+  }
+</script>
+  
+<style src="@/assets/css/forms.css"></style>
