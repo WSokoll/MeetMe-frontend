6418b4f9267df07167221cae009c8369f85dd32d
WSokoll <wladek.sokol01@gmail.com>
2023-04-25 22:06:41 +0200

    Registration - connection to backend (not working)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App).use(router).use(VueAxios, axios)
+
+fetch(process.env.BASE_URL + "config.json")
+  .then((response) => response.json())
+  .then((config) => {
+    app.config.globalProperties.$config = config
+    app.mount("#app")
+  })
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -2,34 +2,34 @@
     <section class="form-section">
       <div class="form-box form-box-register">
         <div class="form-value">
-          <form action="">
+          <form action="" onsubmit="return false;">
             <h2 class="form-title">Register</h2>
             <div class="inputbox">
               <i class="bi bi-person form-icon"></i>
-              <input type="text" placeholder=" " required>
+              <input v-model="username" type="text" placeholder=" " required>
               <label for="">Username</label>
             </div>
             <div class="inputbox">
               <i class="bi bi-person form-icon"></i>
-              <input type="text" placeholder=" " required>
+              <input v-model="firstName" type="text" placeholder=" " required>
               <label for="">First name</label>
             </div>
             <div class="inputbox">
               <i class="bi bi-person form-icon"></i>
-              <input type="text" placeholder=" " required>
+              <input v-model="lastName" type="text" placeholder=" " required>
               <label for="">Last name</label>
             </div>
             <div class="inputbox">
             <i class="bi bi-envelope-at form-icon"></i>
-              <input type="email" placeholder=" " required>
+              <input v-model="email" type="email" placeholder=" " required>
               <label for="">Email</label>
             </div>
             <div class="inputbox">
               <i class="bi bi-lock form-icon"></i>
-              <input type="password" placeholder=" " required>
+              <input v-model="password" type="password" placeholder=" " required>
               <label for="">Password</label>
             </div>
-            <button class="form-button">Register</button>
+            <button @click="register" type="submit" class="form-button">Register</button>
             <div class="login-link">
               <p>Already have an account? <router-link to="/login">Log in</router-link></p>
             </div>
@@ -41,8 +41,43 @@
   
 <script>
   export default {
-    name: 'RegisterView'
+    name: 'RegisterView',
+    data() {
+      return {
+        username: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+      }
+    },
+
+    methods: {
+      register() {
+        this.axios.post(
+          this.$config.BACKEND_URL + "/registration",
+          {
+            username: this.username,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            email: this.email,
+            password: this.password
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "crossDomain": true
+            }
+          }
+        )
+        .then(() => {
+          console.log("success");
+          this.$router.push({path: 'home'})
+        })
+      }
+    }
   }
+
 </script>
   
 <style src="@/assets/css/forms.css"></style>
