ff5ba0efe2a9c2820c8747e863fa39ca4bbca606
WSokoll <wladek.sokol01@gmail.com>
2023-05-02 12:04:30 +0200

    add Leaflet.js + map on home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,18 +11,58 @@
     </div>
   </div>
   
-  <div class="results-section">
+  <div class="results-container">
+
+    <!-- <div class="results-section">
+    </div> -->
+
+    <div class="map-container">
+      <l-map ref="map" v-model:zoom="zoom" :center="[currentLocationLat, currentLocationLng]">
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        ></l-tile-layer>
+
+        <l-marker :lat-lng="[41.8329, -87.7327]">
+          <l-tooltip> Hi! I'm staying here on this location! </l-tooltip>
+        </l-marker>
+      </l-map>
+    </div>
   </div>
 </template>
 
 <script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LMarker, LTooltip } from "@vue-leaflet/vue-leaflet";
+
 
 export default {
   name: 'HomeView',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LTooltip
+  },
   data() {
     return {
-      query: ''
+      query: '',
+      zoom: 13,
+      currentLocationLat: 50.067437,
+      currentLocationLng: 19.916022
     }
+  },
+  created() {
+    // get current location and use it for centering map if access granted
+    const success = (position) => {
+      this.currentLocationLat  = position.coords.latitude;
+      this.currentLocationLng = position.coords.longitude;
+    };
+    const error = (err) => {
+      console.log(err);
+    };
+    navigator.geolocation.getCurrentPosition(success, error);
   }
 }
 </script>
