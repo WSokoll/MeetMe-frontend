efe34cbbcb71307cd79e77c4f2c9cb273cce1da5
WSokoll <wladek.sokol01@gmail.com>
2023-05-04 19:26:27 +0200

    Home view - base version

---
+++ src/components/SearchDetailsComponent.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="card details-card mx-3">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-4">
+                    <img class="rounded img-fluid" style="display: block; margin: auto;" :src="result.image">
+                </div>
+                <div class="col-8">
+                    <div class="d-flex justify-content-between">
+                        <div v-if="tickets_available" class="tickets-info-header">
+                            <i class="bi bi-ticket-detailed" style="margin-right: 5px; color: green"></i>
+                            <span class="tickets-info-header">Tickets available now</span>
+                        </div>
+                        <i @click="closeDetails()" class="bi bi-box-arrow-left" style="cursor: pointer;"></i>
+                    </div>
+                    <h4 class="details-title">{{ result.title }}</h4>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <p v-if="description" style="margin-bottom: 0;">{{ description }}</p><br>
+            <p style="margin-bottom: 5px"><b><i class="bi bi-clock" style="margin-right: 5px;"></i>When?</b></p>
+            <p style="margin-bottom: 0;">{{ result.date.start_date }}&nbsp;&nbsp;<em style="font-size: 0.7em;"><nobr>{{ result.date.when }}</nobr></em></p>
+            <hr style="width:70%;  border: 1px solid; margin-top: 0;">
+            <p style="margin-bottom: 5px"><i class="bi bi-globe-americas" style="margin-right: 5px;"></i><b>Where?</b></p>
+            <p style="margin-bottom: 0;"><nobr>{{ result.address[0] }}</nobr>&nbsp;&nbsp;<em style="font-size: 0.7em;"><nobr>{{ result.address[1] }}</nobr></em></p>
+            <hr style="width:70%;  border: 1px solid; margin-top: 0;">
+            <p><i class="bi bi-link-45deg" style="margin-right: 5px;"></i><b><a :href="result.link" style="text-decoration: none;">Event website</a></b></p>
+            <p v-if="tickets_available" style="margin-bottom: 10px;"><i class="bi bi-ticket-detailed" style="margin-right: 5px;"></i><b>Tickets available at:</b></p>
+            <ul v-if="tickets_available">
+                <li v-for="info in result.ticket_info" :key="info.link"><a :href="info.link" style="text-decoration: none;">{{ info.source }}</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SearchDetailsComponent",
+    props: {
+        result: {}
+    },
+    data() {
+        return{
+            tickets_available: this.result.ticket_info ? true : false,
+            description: this.result.description ? this.result.description : false
+        }
+    },
+    methods: {
+        closeDetails() {
+            this.$parent.resetDetails();
+        }
+    },
+}
+</script>
+
+<style src="@/assets/css/details.css"></style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,73 @@
 <template>
   <div class="navbar search-navbar">
     <div class="container-fluid">
-      <form action="" class="d-flex" role="search">
-        <input v-bind="query" class="search-input" type="text" placeholder="Search" required>
-        <button type="submit" class="btn btn-light search-button">
+      <form action="" class="d-flex" role="search" onsubmit="return false;">
+        <input v-model="query" class="search-input" type="text" placeholder="Search" required>
+        <select class="when-select">
+          <option value="" disabled selected>When</option>
+          <option value="today">Today</option>
+          <option value="tomorrow">Tomorrow</option>
+          <option value="week">This week</option>
+          <option value="next_week">Next week</option>
+          <option value="month">This month</option>
+          <option value="next_month">Next month</option>
+        </select>
+        <button v-if="!loading" @click="searchEvents" type="submit" class="btn btn-light search-button">
           <i class="bi bi-search"></i>
           Search
+        </button>
+        <button v-else type="submit" class="btn btn-light search-button">
+          <i class="bi bi-search"></i>
+          <span style="margin-left: 5px; margin-right: 10px">Search</span>
+          <div class="spinner-border float-end" style="height: 22px; width: 22px" role="status"></div>
         </button>
       </form>
     </div>
   </div>
   
-  <div class="results-container">
+  <div v-if="!resultsShown" class="results-wrapper"></div>
+  <div v-else class="results-wrapper">
 
-    <!-- <div class="results-section">
-    </div> -->
+    <div v-if="!showDetails" class="results-container">
+      <div v-for="item in results" :key="item.id" @click="showEventDetails(item)" class="card result-card mx-2 my-3">
+        <div class="row g-0">
+          <div class="col-2">
+            <img v-if="item.thumbnail" :src="item.thumbnail" class="img-fluid rounded-start" alt="">
+          </div>
+          <div class="col-10">
+            <div class="card-body">
+              {{ item.title }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-    <div class="map-container">
-      <l-map ref="map" v-model:zoom="zoom" :center="[currentLocationLat, currentLocationLng]">
+    <div v-else class="results-container">
+      <SearchDetailsComponent :result="resultDetails" />
+    </div>
+
+    <div class="map-container" v-if="bounds != null">
+      <l-map ref="map" v-model:zoom="zoom" :bounds="bounds" :center="center">
         <l-tile-layer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           layer-type="base"
           name="OpenStreetMap"
         ></l-tile-layer>
-
-        <l-marker :lat-lng="[41.8329, -87.7327]">
-          <l-tooltip> Hi! I'm staying here on this location! </l-tooltip>
+        <l-marker v-for="item in results" :key="item.id" :lat-lng="item.coordinates" @click="showEventDetails(item)">
+          <l-tooltip>{{ item.venue ? item.venue.name : item.title }}</l-tooltip>
         </l-marker>
       </l-map>
     </div>
   </div>
+
 </template>
 
 <script>
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer, LMarker, LTooltip } from "@vue-leaflet/vue-leaflet";
+import SearchDetailsComponent from '../components/SearchDetailsComponent.vue'
+import L from "leaflet"
 
 
 export default {
@@ -43,28 +76,94 @@ export default {
     LMap,
     LTileLayer,
     LMarker,
-    LTooltip
+    LTooltip,
+    SearchDetailsComponent
   },
   data() {
     return {
+      resultsShown: false,
+      loading: false,
       query: '',
-      zoom: 13,
-      currentLocationLat: 50.067437,
-      currentLocationLng: 19.916022
+      results: [],
+      showDetails: false,
+      resultDetails: {},
+      zoom: 11,
+      center: [50.067437, 19.916022],
+      bounds: null,
     }
   },
-  created() {
-    // get current location and use it for centering map if access granted
-    const success = (position) => {
-      this.currentLocationLat  = position.coords.latitude;
-      this.currentLocationLng = position.coords.longitude;
-    };
-    const error = (err) => {
-      console.log(err);
-    };
-    navigator.geolocation.getCurrentPosition(success, error);
+  methods: {
+    searchEvents() {
+      if (this.query != '') {
+        this.resetDetails();
+
+        this.loading = true;
+        let q = this.query.trim().replace(/ /g, "+");
+
+        this.axios.get(
+          this.$config.BACKEND_URL + "/events",
+          {
+            params: {
+              q: q
+            }
+          }
+        )
+        .then((response) => {
+          this.results = response.data;
+          
+          let pinLayer = L.layerGroup();
+          let arrayOfLatLngs = [];
+        
+          for (let i in this.results) {
+            let pin = L.marker(this.results[i].coordinates).bindPopup(this.results[i].title);
+            pin.addTo(pinLayer);
+         
+            arrayOfLatLngs.push(this.results[i].coordinates);
+          }
+          this.bounds = new L.LatLngBounds(arrayOfLatLngs);
+          this.center = [(this.bounds.getNorth() + this.bounds.getSouth())/2, (this.bounds.getEast() + this.bounds.getWest())/2]
+          
+          this.resultsShown = true;
+          this.loading = false;
+        })
+        .catch(error => {
+          this.loading = false;
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            this.errorMessage = error.response.data.message;
+
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser 
+            // and an instance of http.ClientRequest in node.js
+            this.errorMessage = error.request.data.message;
+
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+          }
+        })
+      }
+    },
+    showEventDetails(result) {
+      this.resultDetails = result;
+      this.showDetails = true;
+    },
+    resetDetails(){
+      this.resultDetails = {};
+      this.showDetails = false;
+    }
   }
 }
+
+// async function searchEvents(query) {
+  
+// } 
 </script>
 
 <style src="@/assets/css/home.css"></style>
